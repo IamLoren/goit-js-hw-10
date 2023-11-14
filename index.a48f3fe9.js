9fd01b0ab07608d954af8050b42c429437d763eb
IamLoren <94172173+IamLoren@users.noreply.github.com>
2023-11-14 23:21:10 +0000

    Deploying to gh-pages from @ IamLoren/goit-js-hw-10@e1ce98d9620a91f1856f62c30f2d4d3d6c01da87 🚀

--- index.a48f3fe9.js
+++
@@ -1,2 +0,0 @@
-function e(e){const t=document.createDocumentFragment();return e.forEach((e=>{const n=document.createElement("option");n.value=e.id,n.textContent=e.name,t.appendChild(n)})),t}const t=document.querySelector(".breed-select");fetch("https://api.thecatapi.com/v1/breeds?api_key=live_ibyatNn5rBNHwb6AHoqh0GOz6MVW1AxK9xW3tFtu9yqqZGf7HBgRAGozmXPh2oNq").then((e=>{if(!e.ok)throw new Error(e.status);return e.json()})).then((n=>{const o=e(n);t.appendChild(o)})).catch((e=>console.log(e)));
-//# sourceMappingURL=index.a48f3fe9.js.map
